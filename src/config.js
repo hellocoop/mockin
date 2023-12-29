166e472267f697b06670cb19e2299725d7b07e5a
Dick Hardt <dick.hardt@gmail.com>
2023-12-28 16:46:59 -0800

    change default port to 3333

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@
 const { env } = process
 
 
-export const ISSUER = env.ISSUER || 'http://mockin'
+export const ISSUER = env.ISSUER || 'http://127.0.0.1'
 
 if ('https://issuer.hello.coop' === ISSUER.toLowerCase().replace(/\/$/, "")) {
     throw new Error('ISSUER must not be https://issuer.hello.coop')
 }
 console.log(`mockin ISSUER: ${ISSUER}`)
 
-export const PORT = env.PORT || 3210
+export const PORT = env.PORT || 3333
 export const IP = env.IP || '127.0.0.1'
