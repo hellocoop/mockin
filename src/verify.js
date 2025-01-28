ce1d9f7c34495fda14027dc9026cd2cb4cfdf799
dickhardt <dick.hardt@hello.coop>
2025-01-28 11:01:52 +0000

    fix acceptable typ values, improve debugging

--- src/verify.js
+++ src/verify.js
@@ -28,13 +28,31 @@ const verify = async function (token, audience, nonce) {
         if (payload?.nonce && !nonce)
             return({error:'nonce_required'})
         const key = pems[header?.kid]
-        if (    (header.alg != 'RS256')
-            ||  (header.typ != 'JWT')
-            ||  (payload.iss != ISSUER)
-            ||  (payload.aud != audience)
-            ||  (payload.nonce && payload.nonce != nonce)
-            ||  (!key) )
+        if (header.alg != 'RS256') {
+            console.error('Invalid algorithm: expected RS256, got ', header.alg)
             return({active:false})
+        }
+        const typ = header?.typ?.toLowerCase()
+        if (typ != 'jwt' && typ != 'command+jwt') {
+            console.error('Invalid type: expected JWT or command+jwt, got ', header.typ)
+            return({active:false})
+        }
+        if (payload.iss != ISSUER) {
+            console.error('Invalid issuer: expected ', ISSUER, ' got ', payload.iss)
+            return({active:false})
+        }
+        if (payload.aud != audience) {
+            console.error('Invalid audience: expected ', audience, ' got ', payload.aud)
+            return({active:false})
+        }
+        if (payload.nonce && payload.nonce != nonce) {
+            console.error('Invalid nonce: expected ', nonce, ' got ', payload.nonce)
+            return({active:false})
+        }
+        if (!key) {
+            console.error('Invalid key: ', header.kid)
+            return({active:false})
+        }
         const decoded = jwt.verify(token, key, options)
         // all good if we made it here
         decoded.active = true
